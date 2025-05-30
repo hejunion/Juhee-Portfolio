a08e2f2688d118880d8afb3ed073f3f692b0a508
hejunion <hejunion@gmail.com>
2025-05-30 08:47:00 -0700

    Pic...

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import homeLogo from "../../Assets/home-main-1.jpg";
+import homeLogo from "../../Assets/home-main-2.png";
 import Particle from "../Particle";
 import Home2 from "./Home2";
 import Type from "./Type";
